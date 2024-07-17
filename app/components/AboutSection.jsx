9ba79bd3390a56fc8807821f28895504e4fe9eb1
osstd <omarelshemy94@gmail.com>
2024-07-17 07:46:33 -0400

    u-about

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -90,14 +90,14 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am an aspiring full stack developer with a passion for creating
-            interactive and responsive applications. My expertise includes
-            working with Python, JavaScript, React, Node.js, Express,
-            PostgreSQL, MongoDB, Redis, and Git. As a quick learner, I am
-            constantly seeking opportunities to expand my knowledge and skill
-            set. I thrive in collaborative environments and am excited to
-            contribute to dynamic teams in developing innovative and impactful
-            applications.
+            I'm a software developer who crafts interactive and responsive web
+            applications. Proficient in Python, JavaScript, and SQL. I leverage
+            technologies like React, Node.js, Express, PostgreSQL, MongoDB,
+            Redis and Git to deliver impactful solutions. My track record
+            includes successfully solving real-world problems through multiple
+            project deliveries. I continuously enhance my skills by embracing
+            new challenges. I am always excited to contribute to dynamic
+            teams in developing innovative and impactful applications.
           </p>
           <div className="flex flex-row flex-wrap justify-start mt-8">
             <TabButton
